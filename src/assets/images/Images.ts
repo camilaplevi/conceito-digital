b8c7ef32af0098a3a1bbd1b2b72ea5d15f28e9d4
Camila Pires Levi <camilaplevi@gmail.com>
2025-05-07 00:56:11 -0300

    adição do header

---
+++ src/assets/images/Images.ts
@@ -0,0 +1,5 @@
+import LogoConteito from 'src/assets/images/logo-conceito.png';
+
+export const imagesConceito = {
+  logoConceito: LogoConteito,
+};
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,102 +1,88 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { imagesConceito } from 'src/assets/images/Images'
+import { useRoute } from 'vue-router';
+
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+
+    const rightDrawerOpen = ref(false);
+
+    const toggleRightDrawer = () => {
+      rightDrawerOpen.value = !rightDrawerOpen.value;
+    };
+
+    const menuItems = [
+      { id: 1, path: 'inicio', label: 'Início' },
+      { id: 2, path: 'sobre', label: 'Sobre' },
+      { id: 3, path: 'portfolio', label: 'Portfólio' },
+      { id: 4, path: 'contato', label: 'Contato' }
+    ]
+
+    return {
+      menuItems,
+      route,
+      rightDrawerOpen,
+      toggleRightDrawer,
+
+      imagesConceito
+    }
+  }
+
+})
+</script>
+
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh Lpr fff">
+    <q-header elevated class="bg-black text-white">
+      <q-toolbar class="q-px-md bg-black" style="height: 100px; justify-content: space-between">
+        <img :src="imagesConceito.logoConceito" alt="" style="width: 160px; margin-left: 200px;">
+        <div class="desktop-menu row items-center" style="margin-right: 200px">
+          <ul class="menu-list"
+            style="list-style: none; padding: 0; margin: 0; display: flex; align-items: center; gap: 40px;">
+            <li v-for="item in menuItems" :key="item.id" class="menu-item"
+              style="margin-right: 20px; position: relative;">
+              <router-link :to="item.path" :class="{ 'active-route': route.path === item.path }" class="menu-link"
+                style="text-decoration: none; color: #fff; font-family: Inter; font-size: 17.161px; font-weight: 400; line-height: normal; padding: 5px 0; position: relative; transition: all 0.3s ease;">
+                {{ item.label }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat dense round icon="menu" aria-label="Menu" class="hamburger-menu" @click="toggleRightDrawer"
+          style="display: none; margin-right: 15px" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="rightDrawerOpen" bordered side="right" behavior="mobile" class="bg-white">
+      <img :src="imagesConceito.logoConceito" alt="Logo FireBeauty" class="logo-mobile" />
+      <hr style="width: 90%" />
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item v-for="item in menuItems" :key="item.id" clickable v-ripple :to="item.path"
+          :class="{ active: route.path === item.path }" class="mobile-menu-item">
+          <q-item-section>{{ item.label }}</q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+<style lang="scss">
+@media (max-width: 790px) {
+  .desktop-menu {
+    display: none !important;
   }
-];
 
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
+  .hamburger-menu {
+    display: block !important;
+  }
 }
-</script>
+</style>
